414f087e4fc37b3888675f76d17216c849c1cca4
DukunHRC <64887665+DukunHRC@users.noreply.github.com>
2020-05-07 02:29:06 +0700

    ppppppp

---
+++ Tools-HRC.sh
@@ -0,0 +1,99 @@
+#Jangan Lupa Follow IG||dukun_hrc
+clear
+figlet "Tools Dukun_HRC"
+echo "================================="
+echo "auther    : Dukun_HRC"
+echo "Instagram : @dukun_hrc"
+echo "GitHub    : https://github.com/Dukun_HRC-ID"
+echo "============================================="
+echo 
+echo "Pilih Menu Tools Nya GBLK"
+echo "[1] Animasi Kereta Api"
+echo "[2] Ltc Clickbot"
+echo "[3] Tools V2"
+echo "[4] TUANB4DUTv3"
+echo "[5] Dark-v9.9"
+echo "[6] Tools Hack FB"
+echo "[7] B4J1N64N"
+echo "[8] Install Bahan-Bahan"
+read -p "[?] Pilih : " "pil"
+
+# Batas gan
+if [ $pil = 1 ]
+then
+pkg install sl
+sl
+fi
+
+# batas gan
+if [ $pil = 2 ]
+then
+git clone https://github.com/kyo1337/ltcclickbot
+cd ltcclickbot
+ls
+python main.py +62
+fi
+
+# batas gan
+if [ $pil = 3 ]
+then
+git clone https://github.com/MrXXXTRI3WSL/ToolsV2
+cd ToolsV2
+ls
+python INSTALLERV22.py
+fi
+
+# batas gan
+if [ $pil = 4 ]
+then
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+cd TOOLSINSTALLERv3
+chmod +x TUANB4DUT.sh
+ls
+sh TUANB4DUT.sh
+fi
+
+# batas gan
+if [ $pil = 5]
+then
+git clone https://github.com/bangsat716/Dark-v9.9
+cd Dark-v9.9
+ls
+python2 Dark-v9.9.py
+fi
+
+# batas gan
+if [ $pil = 6 ]
+then
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+ls
+sh new.sh
+fi
+
+# batas gan
+if [ $pil = 7 ]
+then
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+ls
+sh B4J1N64N.sh
+fi
+
+# batas gan
+if [ $pil = 8 ]
+then
+pkg update && pkg upgrade
+pkg install nano
+pkg install python
+pkg install git
+pkg install python2
+pkg install requsts mechanize
+pkg install php
+pkg install pip
+pkg install touch
+pip install bs4
+pip install pyaes
+pip2 install requests mechanize
+fi
+
