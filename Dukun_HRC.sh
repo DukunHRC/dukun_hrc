f9193e626d4016dcebe41588df19f5d1a926d19b
DukunHRC <64887665+DukunHRC@users.noreply.github.com>
2020-05-06 14:34:24 +0700

    Add files via upload

---
+++ Dukun_HRC.sh
@@ -0,0 +1,108 @@
+#Jangan Lupa Follow IG||dukun_hrc
+clear
+figlet "Tools Dukun_HRC"
+echo "================================="
+echo "auther    : Dukun_HRC"
+echo "Instagram : @dukun_hrc"
+echo "GitHub    : https://github.com/Dukun_HRC-ID"
+echo "============================================="
+echo 
+echo "Pilih Menu Tools Nya GBLK"
+echo "[1] Dark FB"
+echo "[2] MBF"
+echo "[3] LITESPAM"
+echo "[4] Hack CCTV"
+echo "[5] Prank Spammer"
+echo "[6] Tools Hack FB"
+echo "[7] Install Bahan-Bahan"
+read -p "[?] Pilih : " "pil"
+
+# Batas gan
+if [ $pil = 1 ]
+then
+git clone https://github.com/B4N954N2-ID/DarkPremium
+cd DarkPremium
+ls
+python2 darkpremium.py
+fi
+
+# batas gan
+if [ $pil = 2 ]
+then
+git clone https://github.com/pirmansx/mbf
+cd mbf
+ls
+python2 MBF.py
+fi
+
+# batas gan
+if [ $pil = 3 ]
+then
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+ls
+sh LITESPAM.sh
+fi
+
+# batas gan
+if [ $pil = 4 ]
+then
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+ls
+python2 scan.py
+fi
+
+# batas gan
+if [ $pil = 5]
+then
+git clone https://github.com/B4N954N2-ID/prank
+cd prank
+ls
+python2 spam.py
+fi
+
+# batas gan
+if [ $pil = 6 ]
+then
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+ls
+sh new.sh
+fi
+
+# batas gan
+if [ $pil = 7 ]
+then
+apt update && apt upgrade
+pkg install nano
+pkg install python
+pkg install git
+pkg install python2
+pkg install requsts mechanize
+pkg install php
+pkg install pip
+pkg install touch
+pip install bs4
+pip install pyaes
+pip2 install requests mechanize
+fi
+
+#Jangan Lupa Follow IG||dukun_hrc
+clear
+figlet "Tools Dukun_HRC"
+echo "================================="
+echo "auther    : Dukun_HRC"
+echo "Instagram : @dukun_hrc"
+echo "GitHub    : https://github.com/Dukun_HRC-ID"
+echo "============================================="
+echo 
+echo "Pilih Menu Tools Nya GBLK"
+echo "[1] Dark FB"
+echo "[2] MBF"
+echo "[3] LITESPAM"
+echo "[4] Hack CCTV"
+echo "[5] Prank Spammer"
+echo "[6] Tools Hack FB"
+echo "[7] Install Bahan-Bahan"
+read -p "[?] Pilih : " "pil"
